c81a8c6a54ff243b6d71d88e65a6e0f5bddb2e14
kaiyaointel <kai.yao@intel.com>
2022-12-09 10:47:28 +0800

    Update and rename download_cifar_kit.py to download_cifar_kit_and_dataset.py

--- cifar/download_cifar_kit_and_dataset.py
+++ cifar/download_cifar_kit_and_dataset.py
@@ -11,3 +11,8 @@ md5 = "c615b172eb42aac01f3a0737540944b1"
 
 # WARNING: this will download and extract a 2.1GiB file, if not already present. Please save the file and avoid re-downloading it.
 download_and_extract_archive(url=url, download_root=os.curdir, extract_root=None, filename=filename, md5=md5, remove_finished=False)
+
+
+# DOWNLOAD DATASET
+from mico_competition import load_cifar10
+full_dataset = load_cifar10(dataset_dir="./cifar10_data/", download=True)
