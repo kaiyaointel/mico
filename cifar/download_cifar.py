0424a93a23ce34e3a6e7505bb614bec6f4177604
kaiyaointel <kai.yao@intel.com>
2022-12-09 10:53:46 +0800

    Update download_cifar.py

--- cifar/download_cifar.py
+++ cifar/download_cifar.py
@@ -6,7 +6,7 @@ import urllib
 from torchvision.datasets.utils import download_and_extract_archive
 
 url = "https://membershipinference.blob.core.windows.net/mico/cifar10.zip?si=cifar10&spr=https&sv=2021-06-08&sr=b&sig=d7lmXZ7SFF4ZWusbueK%2Bnssm%2BsskRXsovy2%2F5RBzylg%3D" 
-filename = "cifar10.zip"
+filename = "cifar10_kit.zip"
 md5 = "c615b172eb42aac01f3a0737540944b1"
 
 # WARNING: this will download and extract a 2.1GiB file, if not already present. Please save the file and avoid re-downloading it.
